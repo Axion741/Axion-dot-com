9307424e8d9d33f28546aadadb30de22028905d4
Unknown <k.ballagher@gmail.com>
2018-12-02 22:39:55 +0000

    Started Blog Construction
    Started building basic structure of a blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,15 +1,57 @@
 import React from 'react';
 import './Blog.css';
+import Headshot from "../Landing/ProfessionalButton/Headshot.jpg"
 
-const Blog = ({onRouteChange}) =>{
+class Blog extends React.Component {
+
+    render() {
 
 
-    return(
-    <div className="masterContainer">
-        <h1>Blog</h1>
-        <h2 onClick={() => onRouteChange('landing')} className="pointer grow">Back</h2>
-    </div>
-);
+
+        return (
+            <div className="blogMasterContainer">
+                <div className="blogContainer">
+                    <div className="postHeader">
+                        <div className="postTitle">
+                          <h1>Example Post</h1>  
+                        </div>
+                        <div className="postHeadFooter">
+                            <div className="postDate">
+                                <h2>01/01/2018</h2>
+                            </div>
+                            <div className="keywords">
+                                <h2>Test, coding</h2>
+                            </div>
+                            <div className="author">
+                                <h2>Posted by Kyle</h2>
+                            </div>
+                            <div className="authorImgParent">
+                                <img className="authorImg" src={Headshot}/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="postBody">
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                    </div>
+                </div>
+                <div className="sidebar">
+                    <h1>Sidebar</h1>
+                    <h1>Sidebar</h1>
+                    <h1>Sidebar</h1>
+                    <h1>Sidebar</h1>
+                    <h1>Sidebar</h1>
+                </div>
+            </div>
+        );
+    }
+
+
 
 
 }
